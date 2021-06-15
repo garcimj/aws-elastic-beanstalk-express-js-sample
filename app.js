72da231a61659ebb8e8a28bf6860a584dbdecbb4
garcimj <mattgarcia556@gmail.com>
2021-06-15 13:38:28 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Final test of AWS pipeline'));
+app.get('/', (req, res) => res.send('Just kidding, this is the Final test of AWS pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
